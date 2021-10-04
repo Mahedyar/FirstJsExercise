afab898d334e20b46996a7a33c7cb1ee606665d3
Mahed Yarmohammadi <Mahedyar@gmail.com>
2021-10-04 20:45:38 +0330

    4 first exercises has been added .

--- firstExerciseScript.js
+++ firstExerciseScript.js
@@ -35,6 +35,76 @@ let i = 0 ;
     }
 }
 
+const maghsum = (number) => {
+    let store = 0 ;
+    let i = 1 ;
+    while ( store < number){
+
+        if (number % i === 0){
+            store = store + i ;
+        }
+        ++i ;
+    }
+    if (store === number){
+        console.log("YES")
+    }else {
+        console.log("NO")
+    }
+}
+
+const Avval = (a) =>{
+    let i = 2
+    while ( i < a) {
+        if (a % i === 0){
+            return false
+        }
+        ++i ;
+    }
+    return true
+}
+
+const avvalShow = (firstNumber , secondNumber) =>{
+    let result = "";
+    let max = Math.max(secondNumber , firstNumber)
+    let min = Math.min(secondNumber,firstNumber)
+
+    while (max > min+1){
+        max-- ;
+        if (Avval(max)===true){
+            result += max + ","
+        }
+    }
+    console.log(result.substring(0, result.length-1))
+}
+
+
+const maxmaker = (number) => {
+   return  10**(number-1) - 1
+}
+
+const numDivider =(number) => {
+    let answer = number.toString(1)
+    console.log(answer)
+    // while (number/10 >= 1){
+    //     storage = (number - (number % 10) ) /10
+    //     console.log(storage)
+    //     number = (number - (number % 10) ) /10
+    // }
+}
+// function passMaker(N){
+//     let max = maxmaker(N)
+//     while (max > 100){
+//
+//     }
+// }
+
+
+numDivider(5)
+
+
+
+
+
 
 
 
